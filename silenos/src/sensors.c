f3314731c2b152c7a9297865f02bfc0a8ca6f7dd
ruptim <timonrupelt@gmx.de>
2025-11-03 09:56:07 +0100

    formatted files

--- silenos/src/sensors.c
+++ silenos/src/sensors.c
@@ -7,29 +7,20 @@ void dwax_alarm_cb(void *arg)
     msg_send_int(&cb_args->msg, cb_args->pid);
 }
 
-
 void reed_nc_callback(void *args)
 {
     (void)args;
     alarm_cb_args_t *msg_args = (alarm_cb_args_t *)args;
 
-    // uint8_t const sensor_type = msg_args->msg.type >> 8;
-    // uint8_t const sensor_id = msg_args->msg.type & 0x00FF;
-    // printf("NC: %d, %d\n", sensor_type, sensor_id);
     msg_send_int(&msg_args->msg, msg_args->pid);
 }
-
 
 void reed_no_callback(void *args)
 {
     (void)args;
     alarm_cb_args_t *msg_args = (alarm_cb_args_t *)args;
-    // uint8_t const sensor_type = msg_args->msg.type >> 8;
-    // uint8_t const sensor_id = msg_args->msg.type & 0x00FF;
-    // printf("NO: %d, %d\n", sensor_type, sensor_id);
     msg_send_int(&msg_args->msg, msg_args->pid);
 }
-
 
 void reed_nc_callback_and_dwax_trigger(void *args)
 {
--- silenos/src/sensors.h
+++ silenos/src/sensors.h
@@ -5,15 +5,15 @@
 #include "dwax509m183x0.h"
 #include "reed_sensor_driver.h"
 
-#define SENSOR_TYPE_ID_DWAX509M183X0 1
-#define SENSOR_TYPE_ID_REED_SWITCH_NC 2
-#define SENSOR_TYPE_ID_REED_SWITCH_NO 3
+#define SENSOR_TYPE_ID_DWAX509M183X0    1
+#define SENSOR_TYPE_ID_REED_SWITCH_NC   2
+#define SENSOR_TYPE_ID_REED_SWITCH_NO   3
 
-#define REED_SENSOR_DEBOUNCE_MS 60
-#define REED_SENSOR_PIN_STATE_OPEN 0 
-#define REED_SENSOR_PIN_STATE_CLOSED 1
-#define REED_SENSOR_NOT_ACTIVATED 0 
-#define REED_SENSOR_ACTIVATED 1 
+#define REED_SENSOR_DEBOUNCE_MS         60
+#define REED_SENSOR_PIN_STATE_OPEN      0
+#define REED_SENSOR_PIN_STATE_CLOSED    1
+#define REED_SENSOR_NOT_ACTIVATED       0
+#define REED_SENSOR_ACTIVATED           1
 
 /*
 * Macro to encode sensor type and global sensor id in a 16-Bit integer.
@@ -26,23 +26,19 @@
 #define ENCODE_SENSOR_TYPE_ID(type, id) ((type) << 8 | (id))
 
 typedef union {
-	reed_sensor_driver_t reed_sensor;
-	dwax509m183x0_t inductive_sensor;
+    reed_sensor_driver_t reed_sensor;
+    dwax509m183x0_t inductive_sensor;
 } sensor_base_type_t;
 
 typedef union {
-	reed_sensor_driver_params_t reed_sensor_params;
-	dwax509m183x0_params_t inductive_sensor_params;
+    reed_sensor_driver_params_t reed_sensor_params;
+    dwax509m183x0_params_t inductive_sensor_params;
 } sensor_base_params_t;
 
-
-typedef struct
-{
+typedef struct {
     kernel_pid_t pid; // thread that receives the msg
     msg_t msg;        // preallocated scratchspace for msg;
 } alarm_cb_args_t;
-
-
 
 void dwax_alarm_cb(void *arg);
 
@@ -67,5 +63,3 @@ void reed_no_callback(void *args);
  * @param args the complete alarm_cb_args_t array with the dwax config at [0] and the nc config at [1].
  */
 void reed_nc_callback_and_dwax_trigger(void *args);
-
-
