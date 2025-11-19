7a5f0ca4a7d8d9c1326a1b57412c2289702089ab
ruptim <timonrupelt@gmx.de>
2025-11-19 12:59:22 +0100

    code formatting

--- custom-modules/gate_monitoring/src/event_processing.h
+++ custom-modules/gate_monitoring/src/event_processing.h
@@ -1,20 +1,18 @@
 #pragma once
 
 #ifdef __cplusplus
-extern C {
+extern C
+{
 #endif
 
-
-/**
+    /**
  * @brief The event receive routine waiting for new sensor events. When a event arrives,
  *        the sensor type is determined to read it's current value and then forwards it 
  *        to be added to the current gate state.  
  * 
  */
-void * await_sensor_events(void* arg);
-
+    void *await_sensor_events(void *arg);
 
 #ifdef __cplusplus
 }
 #endif
-
