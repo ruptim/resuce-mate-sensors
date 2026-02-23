754127293de556ab3adf839d121359b42a0062dd
ruptim <timonrupelt@gmx.de>
2026-02-23 09:42:44 +0100

    new configuration sensor_config code

--- custom-modules/gate_monitoring/src/sensor_config.c
+++ custom-modules/gate_monitoring/src/sensor_config.c
@@ -11,16 +11,16 @@ int init_sensors(kernel_pid_t receive_pid)
 
     int ret = 0;
     /* -------------------- init code for sensor 'sensor_1_reed' -------------------- */
-    alarm_cb_args[SENSOR_1_REED_NC_ID].pid = receive_pid;
-    alarm_cb_args[SENSOR_1_REED_NC_ID].msg.type = ENCODE_SENSOR_TYPE_IDS(0,SENSOR_TYPE_ID_REED_SWITCH_NC,SENSOR_1_REED_NC_ID);
-    alarm_cb_args[SENSOR_1_REED_NC_ID].msg.content.ptr = (void *)&registered_sensors[SENSOR_1_ID];
+    alarm_cb_args[SENSOR_1_REED_NO_ID].pid = receive_pid;
+    alarm_cb_args[SENSOR_1_REED_NO_ID].msg.type = ENCODE_SENSOR_TYPE_IDS(0,SENSOR_TYPE_ID_REED_SWITCH_NO,SENSOR_1_REED_NO_ID);
+    alarm_cb_args[SENSOR_1_REED_NO_ID].msg.content.ptr = (void *)&registered_sensors[SENSOR_1_ID];
     
     /* first cast to specific param type and then to base params type for the array. */
     registered_sensors_params[SENSOR_1_ID] = (sensor_base_params_t) (reed_sensor_driver_params_t) {
-        .nc_pin = GPIO_PIN(1,9),
-        .nc_int_flank = GPIO_BOTH,
-        .nc_callback = reed_nc_callback,
-        .nc_callback_args = (void *)&alarm_cb_args[SENSOR_1_REED_NC_ID],
+        .no_pin = GPIO_PIN(0,8),
+        .no_int_flank = GPIO_BOTH,
+        .no_callback = reed_no_callback,
+        .no_callback_args = (void *)&alarm_cb_args[SENSOR_1_REED_NO_ID],
         .use_external_pulldown = false,
         .debounce_ms = REED_SENSOR_DEBOUNCE_MS };
     
@@ -30,16 +30,16 @@ int init_sensors(kernel_pid_t receive_pid)
     
     
     /* -------------------- init code for sensor 'sensor_2_reed' -------------------- */
-    alarm_cb_args[SENSOR_2_REED_NC_ID].pid = receive_pid;
-    alarm_cb_args[SENSOR_2_REED_NC_ID].msg.type = ENCODE_SENSOR_TYPE_IDS(1,SENSOR_TYPE_ID_REED_SWITCH_NC,SENSOR_2_REED_NC_ID);
-    alarm_cb_args[SENSOR_2_REED_NC_ID].msg.content.ptr = (void *)&registered_sensors[SENSOR_2_ID];
+    alarm_cb_args[SENSOR_2_REED_NO_ID].pid = receive_pid;
+    alarm_cb_args[SENSOR_2_REED_NO_ID].msg.type = ENCODE_SENSOR_TYPE_IDS(1,SENSOR_TYPE_ID_REED_SWITCH_NO,SENSOR_2_REED_NO_ID);
+    alarm_cb_args[SENSOR_2_REED_NO_ID].msg.content.ptr = (void *)&registered_sensors[SENSOR_2_ID];
     
     /* first cast to specific param type and then to base params type for the array. */
     registered_sensors_params[SENSOR_2_ID] = (sensor_base_params_t) (reed_sensor_driver_params_t) {
-        .nc_pin = GPIO_PIN(0,11),
-        .nc_int_flank = GPIO_BOTH,
-        .nc_callback = reed_nc_callback,
-        .nc_callback_args = (void *)&alarm_cb_args[SENSOR_2_REED_NC_ID],
+        .no_pin = GPIO_PIN(0,12),
+        .no_int_flank = GPIO_BOTH,
+        .no_callback = reed_no_callback,
+        .no_callback_args = (void *)&alarm_cb_args[SENSOR_2_REED_NO_ID],
         .use_external_pulldown = false,
         .debounce_ms = REED_SENSOR_DEBOUNCE_MS };
     
@@ -49,16 +49,16 @@ int init_sensors(kernel_pid_t receive_pid)
     
     
     /* -------------------- init code for sensor 'sensor_3_reed' -------------------- */
-    alarm_cb_args[SENSOR_3_REED_NC_ID].pid = receive_pid;
-    alarm_cb_args[SENSOR_3_REED_NC_ID].msg.type = ENCODE_SENSOR_TYPE_IDS(2,SENSOR_TYPE_ID_REED_SWITCH_NC,SENSOR_3_REED_NC_ID);
-    alarm_cb_args[SENSOR_3_REED_NC_ID].msg.content.ptr = (void *)&registered_sensors[SENSOR_3_ID];
+    alarm_cb_args[SENSOR_3_REED_NO_ID].pid = receive_pid;
+    alarm_cb_args[SENSOR_3_REED_NO_ID].msg.type = ENCODE_SENSOR_TYPE_IDS(2,SENSOR_TYPE_ID_REED_SWITCH_NO,SENSOR_3_REED_NO_ID);
+    alarm_cb_args[SENSOR_3_REED_NO_ID].msg.content.ptr = (void *)&registered_sensors[SENSOR_3_ID];
     
     /* first cast to specific param type and then to base params type for the array. */
     registered_sensors_params[SENSOR_3_ID] = (sensor_base_params_t) (reed_sensor_driver_params_t) {
-        .nc_pin = GPIO_PIN(0,4),
-        .nc_int_flank = GPIO_BOTH,
-        .nc_callback = reed_nc_callback,
-        .nc_callback_args = (void *)&alarm_cb_args[SENSOR_3_REED_NC_ID],
+        .no_pin = GPIO_PIN(0,5),
+        .no_int_flank = GPIO_BOTH,
+        .no_callback = reed_no_callback,
+        .no_callback_args = (void *)&alarm_cb_args[SENSOR_3_REED_NO_ID],
         .use_external_pulldown = false,
         .debounce_ms = REED_SENSOR_DEBOUNCE_MS };
     
--- custom-modules/gate_monitoring/src/sensor_config.h
+++ custom-modules/gate_monitoring/src/sensor_config.h
@@ -46,13 +46,13 @@ extern alarm_cb_args_t alarm_cb_args[NUM_UNIQUE_SENSOR_VALUES];
 /* IDs for all contacts per sensor to use with parameters and identification in messages. */
 
 #define SENSOR_1_ID 0
-#define SENSOR_1_REED_NC_ID 0
+#define SENSOR_1_REED_NO_ID 0
 
 #define SENSOR_2_ID 1
-#define SENSOR_2_REED_NC_ID 1
+#define SENSOR_2_REED_NO_ID 1
 
 #define SENSOR_3_ID 2
-#define SENSOR_3_REED_NC_ID 2
+#define SENSOR_3_REED_NO_ID 2
 
 /*  The number of maximum bytes needed for all sensor values. */
 #define SENSORS_MAX_VALUE_BYTES_NEEDED 3
