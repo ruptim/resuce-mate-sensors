15ce8ccbe29a8dc4664cde616f7e83f60dba42f7
ruptim <timonrupelt@gmx.de>
2026-05-12 09:26:34 +0200

    formatted debug output

--- custom-modules/gate_monitoring/src/messages.c
+++ custom-modules/gate_monitoring/src/messages.c
@@ -192,7 +192,7 @@ void send_data(const gate_state_t state, const uint32_t timestamp)
     
     send_lorawan_packet(cbor_buf, cbor_buf_size);
 
-    DEBUG("=> Msg Seq. number: %d. Size of buffer: %d, size of data: %d\n",seq_num,CBOR_BUFFER_SIZE,cbor_buf_size_needed);
+    DEBUG("[DEBG] Msg Seq. number: %d. Size of buffer: %d, size of data: %d\n",seq_num,CBOR_BUFFER_SIZE,cbor_buf_size_needed);
     for (size_t i = 0; i < cbor_buf_size; i++) {
         DEBUG("%02X", cbor_buf[i]);
     }
